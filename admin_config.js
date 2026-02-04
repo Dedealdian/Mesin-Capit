eef8f3a0e5128cbc64b584605e0a648e42225dae
NamaAnda <email-anda@example.com>
2026-02-04 14:12:50 +0700

    Upload Script Capit Warkop69 Sempurna

---
+++ admin_config.js
@@ -0,0 +1,12 @@
+// EDIT DI SINI LEWAT CPANEL
+const MASTER_CONFIG = {
+    // Daftar Kupon dan Hadiahnya
+    // Format: "KODE_KUPON": "NAMA_HADIAH"
+    "KUPON100": "Beruang 1k",
+    "PIALASULTAN": "Piala Diamond 2.5jt",
+    "GIVEAWAY": "Boneka Beruang",
+    "COBAIN": "Zonkk", // Kalau isi "Zonkk", pemain akan meleset
+    
+    // Pesan Loading
+    "teksLoading": "SEDANG MENYIAPKAN MESIN CLAW ULTIMATE..."
+};
